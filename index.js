3c63532909a359fa20dc5b8d70490d72efcf24d3
naezatsuku <naezatsuku653@gmail.com>
2024-07-04 23:03:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function init() {
 
     // カメラを作成
     const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-    camera.position.set(0, 0, 600);
+    camera.position.set(0, 0, 0);
 
     // カメラコントローラーを作成
     const controls = new THREE.OrbitControls(camera, canvasElement);
